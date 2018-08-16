99aa2a243e48d2e17879f6c00670766d492c9dc6
Patrick <patrickmarino2014@gmail.com>
2018-08-15 19:39:55 -0500

    upload and zip

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, render_template, request, url_for, send_from_directory
+from flask import Flask, render_template, request, url_for, send_from_directory, send_file, abort
+import zipfile
 
 app = Flask(__name__)
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
@@ -12,24 +13,33 @@ def home():
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    target = os.path.join(APP_ROOT, request.form['id'] + '/')
-    print(target)
 
-    if not os.path.isdir(target):
-        os.mkdir(target)
-    filenames = []
-    for file in request.files.getlist('file'):
-        print(file)
-        filename = file.filename
-        destination = "".join([target, filename])
-        filenames.append(filename)
-        file.save(destination)
+    filenames=[]
+    for file in os.listdir(APP_ROOT + '/temp'):
+        os.remove(APP_ROOT +'/temp/' + file)
+
+    with zipfile.ZipFile(APP_ROOT + '/photos/' + request.form['id'] + '.zip', 'w') as myzip:
+        for file in request.files.getlist('file'):
+            filenames.append(file.filename)
+            file.save(APP_ROOT + '/temp/ ' + file.filename)
+
+        for name in filenames:
+            myzip.write(APP_ROOT + '/temp/ ' + name, name)
+
+            if os.path.exists(APP_ROOT + '/temp' + name):
+                os.remove(APP_ROOT + '/temp/' + name)
+
     return render_template('success.html', filenames=filenames)
 
 
 @app.route('/orders/<string:ordernum>')
 def orders(ordernum):
-    images_path = "".join([APP_ROOT, '/', ordernum, '/'])
+    print(APP_ROOT)
+    images_path = "".join([APP_ROOT, '/photos/'])
 
-    print(images_path)
-    return send_from_directory(images_path, '*')
+    if os.path.exists(images_path + ordernum + '.zip'):
+
+        return send_file(images_path + ordernum + '.zip', 'application/zip')
+
+    else:
+        abort(404)
